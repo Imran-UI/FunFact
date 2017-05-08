fb73bc0d585f3ecc935bbb42361ca60803855955
imranandroid <imran92shaik@gmail.com>
2017-05-08 15:29:48 -0400

    adding colors dynamically changing background color on button click  using relative layout

--- app/src/main/java/com/example/imran/funfact/FunFactActivity.java
+++ app/src/main/java/com/example/imran/funfact/FunFactActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class FunFactActivity extends AppCompatActivity {
@@ -11,8 +12,12 @@ public class FunFactActivity extends AppCompatActivity {
      private  TextView mFactText;
      private Button mNewFact;
      private String Fact;
+     private int color;
+     private RelativeLayout mRelativeLayout;
 
     private FactBook factbook = new FactBook();
+    private FactColor factcolor = new FactColor();
+
 
 
     @Override
@@ -22,11 +27,15 @@ public class FunFactActivity extends AppCompatActivity {
 
         mFactText = (TextView) findViewById(R.id.facttext);
         mNewFact = (Button) findViewById(R.id.btn_newfact);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.activity_fun_fact);
+        mFactText.setText(factbook.getFact());
 
         mNewFact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
               Fact = factbook.getFact();
+              color = factcolor.getColor();
+                mRelativeLayout.setBackgroundColor(color);
                 mFactText.setText(Fact);
             }
         });
